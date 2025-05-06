3e35b2b25147ebc6a28cf5e33ee1a01a63ee9cd8
lucashthiele <lucasthiele0@outlook.com>
2025-05-06 17:05:38 -0300

    fix: mock workout service

--- src/test/java/com/gympulse/workoutservice/application/handlers/CreateWorkoutHandlerTest.java
+++ src/test/java/com/gympulse/workoutservice/application/handlers/CreateWorkoutHandlerTest.java
@@ -13,24 +13,32 @@ import java.util.List;
 import java.util.UUID;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @ActiveProfiles("test")
 public class CreateWorkoutHandlerTest {
     CreateWorkoutHandler handler;
+    WorkoutService workoutService;
 
     @BeforeEach
     void setUp() {
-        handler = new CreateWorkoutHandler(new WorkoutService());
+        workoutService = mock(WorkoutService.class);
+        handler = new CreateWorkoutHandler(workoutService);
     }
 
     @Test
     void givenValidWorkoutShouldReturnUUID() throws Exception {
         var workoutReq = new WorkoutRequest("workout 1", List.of(UUID.randomUUID(), UUID.randomUUID(), UUID.randomUUID()));
         var command = new CreateWorkoutCommand(workoutReq);
+        UUID expectedId = UUID.randomUUID();
+        when(workoutService.create(any())).thenReturn(expectedId);
 
         UUID result = handler.handle(command);
 
         assertThat(result).isNotNull();
+        assertThat(result).isEqualTo(expectedId);
     }
 }
